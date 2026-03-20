e558ae131f4abb2d4ca1c3390e513e4bcf6362fa
ntsekouras <ntsekouras@outlook.com>
2026-03-20 18:46:22 +0200

    Experimental: Add `revisions` panel

--- packages/editor/src/components/sidebar/index.js
+++ packages/editor/src/components/sidebar/index.js
@@ -33,6 +33,7 @@ import SidebarHeader from './header';
 import TemplateContentPanel from '../template-content-panel';
 import TemplatePartContentPanel from '../template-part-content-panel';
 import { MediaMetadataPanel } from '../media';
+import PostRevisionsPanel from '../post-revisions-panel';
 import RevisionBlockDiffPanel from '../revision-block-diff';
 import useAutoSwitchEditorSidebars from '../provider/use-auto-switch-editor-sidebars';
 import { sidebars } from './constants';
@@ -132,6 +133,10 @@ const SidebarContent = ( {
 							{ isRevisionsMode && <RevisionFieldsDiffPanel /> }
 							{ ! isRevisionsMode && (
 								<>
+									{ window?.__experimentalDataFormInspector &&
+										[ 'post', 'page' ].includes(
+											postType
+										) && <PostRevisionsPanel /> }
 									<PluginDocumentSettingPanel.Slot />
 									<TemplateContentPanel />
 									<TemplatePartContentPanel />
